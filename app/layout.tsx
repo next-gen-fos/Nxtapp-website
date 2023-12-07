d393a1a2b55340998d7fea599aa1ed609d479b44
Phoenix <ayomikuntemitope.araoye@gmail.com>
2023-12-07 09:08:04 +0200

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Space_Grotesk } from 'next/font/google';
 import './Styles/globals.css';
 import Navbar from './Components/Navbar';
 import Footer from '@/app/Components/Footer';
+import NewNav from '@/app/Components/NewNav';
 
 const SpaceGrotesk = Space_Grotesk({ subsets: ['latin'] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={SpaceGrotesk.className}>
         {/* <Navbar /> */}
+        <NewNav />
         {children}
         <Footer />
       </body>
